23eb067dc7b07212badcc191d58455a8ace4170b
Leessonomy <leksutionast@gmail.com>
2020-04-23 02:11:17 +1000

    Updates

---
+++ precache-manifest.27ba349def0c7ff2852be20d683ae6ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a0ca01102028f58c754f31bdea28f4a",
+    "url": "/task-game/index.html"
+  },
+  {
+    "revision": "3e76ff7070ec4010a585",
+    "url": "/task-game/static/css/main.984629ac.chunk.css"
+  },
+  {
+    "revision": "c81b63aae6bcc0681ada",
+    "url": "/task-game/static/js/2.af61028b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/task-game/static/js/2.af61028b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e76ff7070ec4010a585",
+    "url": "/task-game/static/js/main.26457b19.chunk.js"
+  },
+  {
+    "revision": "d0f4e28b2ea21cb5a8b3",
+    "url": "/task-game/static/js/runtime-main.47697e28.js"
+  }
+]);
